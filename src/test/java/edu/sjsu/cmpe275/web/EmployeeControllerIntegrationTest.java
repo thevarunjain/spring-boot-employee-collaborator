6ee868939b081a35c614d9793c1ae020f22f95fb
Sheena Gupta <sheena.gupta.in@gmail.com>
2019-04-14 12:12:20 -0700

    employee Test cases

--- src/test/java/edu/sjsu/cmpe275/web/EmployeeControllerIntegrationTest.java
+++ src/test/java/edu/sjsu/cmpe275/web/EmployeeControllerIntegrationTest.java
@@ -1,32 +1,41 @@
 package edu.sjsu.cmpe275.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.sjsu.cmpe275.Main;
-import edu.sjsu.cmpe275.domain.entity.Employee;
-import edu.sjsu.cmpe275.domain.repository.EmployeeRepository;
-import edu.sjsu.cmpe275.web.model.response.EmployeeDto;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-import java.util.Optional;
-
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Optional;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.sjsu.cmpe275.Main;
+import edu.sjsu.cmpe275.domain.entity.Employee;
+import edu.sjsu.cmpe275.domain.repository.EmployeeRepository;
+import edu.sjsu.cmpe275.web.model.response.EmployeeDto;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class EmployeeControllerIntegrationTest {
 
     @Autowired
@@ -39,8 +48,8 @@ public class EmployeeControllerIntegrationTest {
     private ObjectMapper objectMapper;
 
     @Test
-    public void createEmployee_on_valid_parameter_should_save_employee() throws Exception {
-        final MvcResult mvcResult = mockMvc.perform(post("/employee?name=rahul&email=pawarrb@gmail.com&employerId=100"))
+    public void b_createEmployee_on_valid_parameter_should_save_employee() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(post("/employee?name=rahul&email=pawarrb@gmail.com&employerId=1"))
                 .andExpect(status().isCreated())
                 .andDo(print())
                 .andExpect(jsonPath("$.id", is(notNullValue())))
@@ -57,27 +66,54 @@ public class EmployeeControllerIntegrationTest {
     }
 
     @Test
-    public void createEmployee_on_invalid_email_should_return_bad_request() throws Exception {
-        mockMvc.perform(post("/employee?name=rahul&email=pawarrbgmail.com&employerId=100"))
+    public void c_createEmployee_on_invalid_email_should_return_bad_request() throws Exception {
+        mockMvc.perform(post("/employee?name=rahul&email=pawarrbgmail.com&employerId=1"))
                 .andExpect(status().isBadRequest())
                 .andDo(print());
     }
 
     @Test
-    public void createEmployee_with_existing_email_should_return_bad_request() throws Exception {
-        mockMvc.perform(post("/employee?name=rahul&email=pawarrb@gmail.com&employerId=100"))
-                .andExpect(status().isCreated())
-                .andDo(print());
+    public void d_createEmployee_with_existing_email_should_return_bad_request() throws Exception {
+        //mockMvc.perform(post("/employee?name=rahul&email=pawarrb@gmail.com&employerId=1"))
+        //        .andExpect(status().isCreated())
+        //        .andDo(print());
 
-        mockMvc.perform(post("/employee?name=mayur&email=pawarrb@gmail.com&employerId=100"))
+        mockMvc.perform(post("/employee?name=mayur&email=pawarrb@gmail.com&employerId=1"))
                 .andExpect(status().isBadRequest())
                 .andDo(print());
     }
 
     @Test
-    public void createEmployee_on_null_name_should_return_bad_request() throws Exception {
-        mockMvc.perform(post("/employee?name=&email=pawarrb@gmail.com&employerId=100"))
+    public void e_createEmployee_on_null_name_should_return_bad_request() throws Exception {
+        mockMvc.perform(post("/employee?name=&email=pawarrb@gmail.com&employerId=1"))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
+    }
+    
+    @Test
+    @Sql("/db/employer_with_existing_employee.sql")
+    public void a_deleteEmployee_validRequest_shouldReturnOK() throws Exception {
+        mockMvc.perform(delete("/employee/1"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+    
+    @Test
+    @Sql("/db/employee_employer_update.sql")
+    public void ab_updateEmployee_channge_employer_should_update_employee() throws Exception {
+        mockMvc.perform(put("/employee/2?name=sheena&email=sheena@gmail.com&employerId=2"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.manager", is(nullValue())));
+    }
+    
+    @Test
+    public void f_updateEmployee_on_valid_parameter_should_update_employee() throws Exception {
+        mockMvc.perform(put("/employee?name=rahul123&email=pawarrb@gmail.com&employerId=1"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.id", is(notNullValue())))
+                .andExpect(jsonPath("$.name", is("rahul123")))
+                .andReturn();
     }
 }
---
+++ src/test/resources/db/employee_employer_update.sql
@@ -0,0 +1,5 @@
+INSERT INTO EMPLOYER (id, name) VALUES (1, 'Google');
+INSERT INTO EMPLOYER (id, name) VALUES (2, 'Adobe');
+INSERT INTO EMPLOYEE (id, name, email, employer_id) VALUES (1, 'mayur', 'mayur.barge@sjsu.edu', 1);
+INSERT INTO EMPLOYEE (id, name, email, employer_id,manager_id) VALUES (2, 'sheena', 'sheena@sjsu.edu', 1,1);
+INSERT INTO EMPLOYEE (id, name, email, employer_id,manager_id) VALUES (3, 'varun', 'varun@sjsu.edu', 1,2);
